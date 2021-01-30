6eccb4629d4d743e2a9ab7f7559209a361d891f8
Hayden Kan <hkan@bayviewglen.ca>
2021-01-29 23:02:08 -0500

    Day one homework

---
+++ Unit One - Intro to Java/src/week1/DayOne.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class DayOne {
+    public static void main(String[] args) {
+        System.out.println("H \t H \t K      K \nH \t H \t K    K \nH \t H \t K  K \nHHHHHHHHHH  \t KK \nH \t H \t K  K \nH \t H \t K    K \nH \t H \t K      K");
+
+        System.out.println("\n\n\tO \tO \n\tO \tO \n\n  O                   O\n    O               O\n      O  O  O  O  O");
+
+        System.out.println("\n\n671 Lincoln Avenue, Winnetka, IL");
+
+        System.out.println("\n\n///////////////////////////////////////////////////////////////////// \n/								/\n/		      Our Lady of Mercy Hospital			/\n/		      -------------------------------------			/\n/								/\n/			1306 Central City			/\n/		           Phone (416) 555-1212			/\n/								/\n/////////////////////////////////////////////////////////////////////");
+    }
+        
+}
--- Unit One - Intro to Java/src/week1/ExampleOne.java
+++ Unit One - Intro to Java/src/week1/ExampleOne.java
@@ -1,12 +1,16 @@
 package week1;
 
-/**
- * ExampleOne
- */
+/*
+ExampleOne is the name of the class. It has the same name as the file.
+Classes are meant to model an object's behaviour.
+The naming convention for classes is uppercase on the first letter of each word
+*/
 
 public class ExampleOne {
 
     public static void main(String[] args) {
-        System.out.println("welcome to ics");
+        System.out.println("Welcome to ICS");
     }
+    
 }
+
